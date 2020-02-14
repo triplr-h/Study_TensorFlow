ac897f5630b3d4698d6e27e42055ada92514b059
triplr-h <hhh2738@sjtu.edu.cn>
2020-02-14 10:38:15 +0800

    增加了输出版本和路径信息

--- test_install.py
+++ test_install.py
@@ -5,4 +5,6 @@ hello = tf.constant('Hello, TensorFlow!')
 sess = tf.compat.v1.Session()
 print(sess.run(hello))
 
+print(tf.__version__)
+print(tf.__path__)
 #在路径C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.2\bin 下，把cudart64_102.dll文件改名为cudart64_101.dll才能够加载成功，版本不对应
